fd0057fbdaa5ca09757afd876a0a25483c6845b7
Puneet Sharma <puni199711@gmail.com>
2024-10-17 11:33:10 +0530

    final changes with form

--- js/main.js
+++ js/main.js
@@ -48,26 +48,11 @@ $(function() {
     });
 
 
-    // porfolio filterizr
-    $('.filter-container').imagesLoaded( function() {
-        var filterizr = $('.filter-container').filterizr();
-    });
+  
 
-    // portfolio filter
-    $('.portfolio-filter-menu li').on('click', function() {
-        $('.portfolio-filter-menu li').removeClass('active');
-        $(this).addClass('active');
-    });
+  
 
     // portfolio magnific popup
-    $('.portfolio').each(function() { // the containers for all your galleries
-        $(this).magnificPopup({
-            delegate: '.portfolio-popup', // the selector for portfolio item
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    });
+   
 
 });
